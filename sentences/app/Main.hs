824fdbad31820c5a13d041a474e60da899a5c6ca
mikeyT42 <mrmojorisen0@gmail.com>
2024-08-29 23:50:19 -0400

    Did some proper formatting

--- sentences/app/Main.hs
+++ sentences/app/Main.hs
@@ -1,16 +1,16 @@
 module Main where
 
-import System.Process (system)
 import System.Exit
+import System.Process (system)
 
 main :: IO ()
 main = do
-    errorCode <- system "clear"
-    case errorCode of
-        ExitSuccess -> do
-            putStrLn "---------------------------------------------------------"
-            putStrLn "\tWelcome To the Sentence Data Aggregator."
-            putStrLn "---------------------------------------------------------"
-        ExitFailure code -> do
-            putStrLn $ "Failed to clear the screen: " ++ show code
-            exitFailure
+  errorCode <- system "clear"
+  case errorCode of
+    ExitSuccess -> do
+      putStrLn "---------------------------------------------------------"
+      putStrLn "\tWelcome To the Sentence Data Aggregator."
+      putStrLn "---------------------------------------------------------"
+    ExitFailure code -> do
+      putStrLn $ "Failed to clear the screen: " ++ show code
+      exitFailure
