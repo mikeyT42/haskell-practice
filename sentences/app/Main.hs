b314d5e80da9b9fe871d2bb433d00286ffe06b00
mikeyT42 <mrmojorisen0@gmail.com>
2024-08-29 23:36:39 -0400

    Ok, now I am finally starting to get somewhere.

--- sentences/app/Main.hs
+++ sentences/app/Main.hs
@@ -1,8 +1,16 @@
 module Main where
 
 import System.Process (system)
+import System.Exit
 
 main :: IO ()
 main = do
-    _ <- system "clear"
-    putStrLn "Hello, Haskell!"
+    errorCode <- system "clear"
+    case errorCode of
+        ExitSuccess -> do
+            putStrLn "---------------------------------------------------------"
+            putStrLn "\tWelcome To the Sentence Data Aggregator."
+            putStrLn "---------------------------------------------------------"
+        ExitFailure code -> do
+            putStrLn $ "Failed to clear the screen: " ++ show code
+            exitFailure
