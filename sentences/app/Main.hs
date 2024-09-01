ee43ebbfb366b8e500d53cb2ccd8ac9559f8cde3
mikeyT42 <mrmojorisen0@gmail.com>
2024-08-31 23:28:17 -0400

    Not a compilable program... It's just too late to wrap my head around this syntax...

--- sentences/app/Main.hs
+++ sentences/app/Main.hs
@@ -3,14 +3,24 @@ module Main where
 import System.Exit
 import System.Process (system)
 
+data RecursionControl = CONTINUE | STOP deriving (Enum)
+
 main :: IO ()
 main = do
   errorCode <- system "clear"
   case errorCode of
+    ExitFailure code -> do
+      putStrLn $ "Failed to clear the screen: " ++ show code
+      exitFailure
     ExitSuccess -> do
       putStrLn "---------------------------------------------------------"
       putStrLn "\tWelcome To the Sentence Data Aggregator."
       putStrLn "---------------------------------------------------------"
-    ExitFailure code -> do
-      putStrLn $ "Failed to clear the screen: " ++ show code
-      exitFailure
+      inputRecursion
+      putStrLn "---------------------------------------------------------------"
+      putStrLn "\t\tThank you and goodbye."
+      putStrLn "---------------------------------------------------------------"
+
+-- /////////////////////////////////////////////////////////////////////////////
+inputRecursion :: IO () -> RecursionControl
+inputRecursion = 
